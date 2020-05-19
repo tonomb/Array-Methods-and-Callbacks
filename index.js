f56030eb0b321f7408dc27a0c7ec8197e311c6b6
antonio martinez baez <tono.mtzb@gmail.com>
2020-05-19 15:20:03 -0500

    completed task 5

--- index.js
+++ index.js
@@ -57,13 +57,22 @@ function getYears(callback) {
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
+function getWinners(callback) {
+    const winners = [];
+    callback(fifaData).forEach(function(game){
+        if(game['Home Team Goals'] > game['Away Team Goals']){
+            winners.push(game['Home Team Name'])
+        } else if(game['Home Team Goals'] < game['Away Team Goals']){
+            winners.push(game['Away Team Name'])
+        } else{
+            winners.push(game['Win conditions'])
+        }
+    })    
 
-    /* code here */
-
+    return winners
 };
 
-getWinners();
+// console.log(getWinners(getFinals));
 
 /* Task 6: Implement a higher-order function called `getWinnersByYear` that accepts the following parameters and returns a set of strings "In {year}, {country} won the world cup!" 
 
